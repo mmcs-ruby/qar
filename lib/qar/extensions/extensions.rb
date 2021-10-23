9d587e3bd515eac0e86d43b6c7942c2995eff67a
COOLIRON2311 <ignatenko.ivan.23112000@gmail.com>
2021-10-23 16:06:38 +0300

    Refactored Matrix.kronecker

--- lib/qar/extensions/extensions.rb
+++ lib/qar/extensions/extensions.rb
@@ -6,30 +6,37 @@ class Complex
   end
 end
 
+# Integer class round extension
 class Integer
   def round(num)
     num
   end
 end
 
+# Matrix class product and print extensions
 class Matrix
   class << self
+    # Computes Kronecker product of two given matrices
     def kronecker(a, b)
       c = Matrix.zero(a.row_count * b.row_count, a.column_count * b.column_count)
-      x = 0
-      a.row_count.times do |i|
-        y = 0
-        a.column_count.times do |j|
-          b.row_count.times do |p|
-            b.column_count.times do |q|
-              c[i + p + x, j + q + y] = b[p, q] * a[i, j]
-            end
-          end
-          y += b.column_count - 1
+      m = [a.row_count, b.row_count].max
+      n = [a.column_count, b.column_count].max
+      (0...a.row_count * b.row_count).each do |i|
+        (0...a.column_count * b.column_count).each do |j|
+          c[i, j] = a[i / m, j / n] * b[i % m, j % n]
         end
-        x += b.row_count - 1
       end
       c
+    end
+  end
+
+  # Prints a matrix
+  def print
+    (0...row_count).each do |i|
+      (0...column_count).each do |j|
+        Kernel.print "#{self[i, j]}\t"
+      end
+      puts
     end
   end
 end
