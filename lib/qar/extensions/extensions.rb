ac3d9187d2e06d0be7631b0711f6d4eb0c900ee8
COOLIRON2311 <ignatenko.ivan.23112000@gmail.com>
2021-10-28 16:54:48 +0300

    Reverted to older version for better performance

--- lib/qar/extensions/extensions.rb
+++ lib/qar/extensions/extensions.rb
@@ -19,12 +19,18 @@ class Matrix
     # Computes Kronecker product of two given matrices
     def kronecker(a, b)
       c = Matrix.zero(a.row_count * b.row_count, a.column_count * b.column_count)
-      m = [a.row_count, b.row_count].max
-      n = [a.column_count, b.column_count].max
-      (0...a.row_count * b.row_count).each do |i|
-        (0...a.column_count * b.column_count).each do |j|
-          c[i, j] = a[i / m, j / n] * b[i % m, j % n]
+      x = 0
+      a.row_count.times do |i|
+        y = 0
+        a.column_count.times do |j|
+          b.row_count.times do |p|
+            b.column_count.times do |q|
+              c[i + p + x, j + q + y] = b[p, q] * a[i, j]
+            end
+          end
+          y += b.column_count - 1
         end
+        x += b.row_count - 1
       end
       c
     end
